61a820668211542bcda88508626d89b47e3d0a71
James Coliz <jcoliz@outlook.com>
2022-03-31 16:00:10 -0700

    build: New empty project for tests

---
+++ YoFi.Experiments.WebApi.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YoFi.Experiments.WebApi.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
