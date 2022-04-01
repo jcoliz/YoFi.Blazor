7d88974416cfec592585b1e858fd00f81408687e
James Coliz <jcoliz@outlook.com>
2022-04-01 12:12:16 -0700

    style: Remove and sort usings

--- YoFi.Experiments.WebApi.Tests/Tests/ReportApiTests.cs
+++ YoFi.Experiments.WebApi.Tests/Tests/ReportApiTests.cs
@@ -1,13 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
-using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
-using YoFi.AspNet.Data;
 using YoFi.Core.Reports;
 using YoFi.Tests.Integration.Helpers;
 
