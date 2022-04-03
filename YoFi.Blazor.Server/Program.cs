07dbf868e7d4e3fbe2d9271f8cec3042effa0537
James Coliz <jcoliz@outlook.com>
2022-04-03 15:53:43 -0700

    feat: Combine client UI into server project

--- YoFi.Blazor.Server/Program.cs
+++ YoFi.Blazor.Server/Program.cs
@@ -1,17 +1,51 @@
+using Common.DotNet;
+using Microsoft.EntityFrameworkCore;
+using YoFi.Blazor.Server.Data;
+using YoFi.Core;
+using YoFi.Core.Reports;
+using YoFi.Core.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("DataSource=bin\\sqlite.db"));
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddSingleton<IClock>(new SystemClock());
+builder.Services.AddScoped<IDataContext, ApplicationDbContext>();
+builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+builder.Services.AddScoped<IReportEngine, ReportBuilder>();
 
 var app = builder.Build();
 
+using (var serviceScope = app.Services.GetService<IServiceScopeFactory>().CreateScope())
+{
+    var context = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    context.Database.EnsureCreated();
+}
+
 // Configure the HTTP request pipeline.
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
 app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
 
-app.UseAuthorization();
+app.UseRouting();
 
+app.MapRazorPages();
 app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
