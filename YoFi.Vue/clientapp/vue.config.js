41d2fe96d07d0bd643098cd05cb2f2140b8d24f0
James Coliz <jcoliz@outlook.com>
2022-04-05 14:21:49 -0700

    build(vue): Add wireapi proxy to vue config

--- YoFi.Vue/clientapp/vue.config.js
+++ YoFi.Vue/clientapp/vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-});
+    devServer: {
+      proxy: {
+        '^/wireapi': {
+          target: 'http://localhost:5003',
+          ws: true,
+          changeOrigin: true
+        }
+      }
+    }
+  });
