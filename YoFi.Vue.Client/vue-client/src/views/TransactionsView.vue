f75dfc931b97caae62f4c2c8b92f6c202ad2c5a8
James Coliz <jcoliz@outlook.com>
2022-04-04 21:15:56 -0700

    feat(vue): Attempt to load transactions index. Fails

--- YoFi.Vue.Client/vue-client/src/views/TransactionsView.vue
+++ YoFi.Vue.Client/vue-client/src/views/TransactionsView.vue
@@ -1,10 +1,36 @@
 <script setup>
 import PageNavBar from '@/components/PageNavBar.vue';
+import wireapiclient from '@/WireApiClient.js';
 </script>
 
 <template>
   <PageNavBar title="Transactions"/>
 </template>
+
+<script>
+export default {
+    data() {
+      return {
+        loading: false,
+        wqr: {}
+      };
+    },
+    async created() {
+      this.getList()
+    },    
+    methods: {
+      async getList() {
+        this.loading = true
+
+        try {
+          this.wqr = await wireapiclient.listTransactions()
+        } finally {
+          this.loading = false
+        }
+      }
+    }
+}
+</script>
 
 <style>
 
