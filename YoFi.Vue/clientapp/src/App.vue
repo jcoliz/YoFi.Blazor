cd3a6bccb809a473a0b30b05ddfbc33f891a7afb
James Coliz <jcoliz@outlook.com>
2022-04-05 18:20:08 -0700

    fix(vue): Tweak spacing on page navbar

--- YoFi.Vue/clientapp/src/App.vue
+++ YoFi.Vue/clientapp/src/App.vue
@@ -42,12 +42,7 @@ export default {
 }
 
 nav {
-  padding: 30px;
-
   a {
-    font-weight: bold;
-    color: #2c3e50;
-
     &.router-link-exact-active {
       color: #42b983;
     }
--- YoFi.Vue/clientapp/src/components/PageNavBar.vue
+++ YoFi.Vue/clientapp/src/components/PageNavBar.vue
@@ -1,5 +1,8 @@
 <template>
-  <nav class="navbar navbar-light navbar-page mb-2" data-test-id="page-navbar">
+  <nav
+    class="navbar navbar-light navbar-page mb-2 px-2"
+    data-test-id="page-navbar"
+  >
     <div class="container-fluid">
       <span class="navbar-brand h1 mt-1" data-test-id="title">{{ title }}</span>
     </div>
