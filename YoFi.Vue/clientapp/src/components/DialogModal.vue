7438485ee6638fe0d8255847f22e6dd4e67f06ee
James Coliz <jcoliz@outlook.com>
2022-04-07 21:13:25 -0700

    feat(vue): Add new basic DialogModal component

---
+++ YoFi.Vue/clientapp/src/components/DialogModal.vue
@@ -0,0 +1,50 @@
+<template>
+  <div data-test-id="DialogModal" class="modal modal-fade" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <slot name="header">
+            <h5 class="modal-title">{{this.$props.title}}</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </slot>
+        </div>
+        <div class="modal-body">
+          <slot></slot>
+        </div>
+        <div class="modal-footer">
+          <slot name="footer">
+            <button
+              type="button"
+              class="btn btn-outline-secondary"
+              data-bs-dismiss="modal"
+            >
+              Cancel
+            </button>
+            <button type="button" class="btn btn-primary">Save</button>
+          </slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DialogModal",
+  props: {
+    title: String,
+  },
+  emits: {
+    newPage: (p) => {
+      return p > 0;
+    }
+  }
+};
+</script>
+
+<style></style>
--- YoFi.Vue/clientapp/src/views/AboutView.vue
+++ YoFi.Vue/clientapp/src/views/AboutView.vue
@@ -2,6 +2,7 @@
 import HelloWorld from "@/components/HelloWorld.vue";
 import PageNavBar from "@/components/PageNavBar.vue";
 import PageActions from "@/components/PageActions.vue";
+import DialogModal from "@/components/DialogModal.vue";
 </script>
 
 <template>
@@ -10,22 +11,37 @@ import PageActions from "@/components/PageActions.vue";
       <PageActions />
     </PageNavBar>
     <div class="home">
-      <img alt="Vue logo" src="../assets/logo.png" />
+      <p><img alt="Vue logo" src="../assets/logo.png" /></p>
       <h2 v-if="$route.params.id">{{ $route.params.id }}</h2>
+
+      <!-- Button trigger modal -->
+      <button
+        data-bs-target="#exampleModal"
+        data-bs-toggle="modal"
+        type="button"
+        class="btn btn-primary mb-4"
+      >
+        Launch demo modal
+      </button>
+
       <HelloWorld msg="Welcome to Your Vue.js App" />
+      <DialogModal id="exampleModal" title="About" class="hide">
+        <p>This is an interesting app!</p>
+      </DialogModal>
+
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "AboutView",
   pageTitle: "About",
   components: {
     HelloWorld,
     PageNavBar,
-    PageActions
+    PageActions,
+    DialogModal
   },
   async beforeCreate() {
     document.title = "About - YoFi";
